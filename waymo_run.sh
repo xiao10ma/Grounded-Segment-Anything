79f5e36d9f29f603e64b8a62f6a11b797f4d454b
Zipei Ma <mazp@mail2.sysu.edu.cn>
2025-04-08 12:55:05 +0800

    Update waymo_run.sh

--- waymo_run.sh
+++ waymo_run.sh
@@ -1,46 +1,58 @@
-# text_prompt: [car, bus. truck, pedestrian]
-# --text_prompt "car"
-# --text_prompt "bus. truck"
-# --text_prompt "pedestrian"
+# 以下脚本将分别使用三种文本提示来处理图像
+# 1. car
+# 2. bus. truck
+# 3. pedestrian
 
-# GPU 0 处理相机0的图像
-CUDA_VISIBLE_DEVICES=0 python grounded_sam_demo.py \
-    --config GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py \
-    --grounded_checkpoint groundingdino_swint_ogc.pth \
-    --sam_checkpoint sam_vit_h_4b8939.pth \
-    --image_dir /HDD_DISK/users/mazipei/BZG/dataset/703/images \
-    --output_dir  /HDD_DISK/users/mazipei/BZG/dataset/703/seg_npy \
-    --box_threshold 0.3 \
-    --text_threshold 0.25 \
-    --text_prompt "car" \
-    --device "cuda" \
-    --file_pattern "*_0.png" &
+# 定义要使用的文本提示数组
+TEXT_PROMPTS=("car" "bus. truck" "pedestrian")
 
-# GPU 1 处理相机1的图像
-CUDA_VISIBLE_DEVICES=1 python grounded_sam_demo.py \
-    --config GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py \
-    --grounded_checkpoint groundingdino_swint_ogc.pth \
-    --sam_checkpoint sam_vit_h_4b8939.pth \
-    --image_dir /HDD_DISK/users/mazipei/BZG/dataset/703/images \
-    --output_dir  /HDD_DISK/users/mazipei/BZG/dataset/703/seg_npy \
-    --box_threshold 0.3 \
-    --text_threshold 0.25 \
-    --text_prompt "car" \
-    --device "cuda" \
-    --file_pattern "*_1.png" &
+# 循环处理每个文本提示
+for text_prompt in "${TEXT_PROMPTS[@]}"; do
+    echo "处理文本提示: $text_prompt"
+    
+    # GPU 0 处理相机0的图像
+    CUDA_VISIBLE_DEVICES=5 python grounded_sam_demo.py \
+        --config GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py \
+        --grounded_checkpoint groundingdino_swint_ogc.pth \
+        --sam_checkpoint sam_vit_h_4b8939.pth \
+        --image_dir /HDD_DISK/users/mazipei/BZG/dataset/017/images \
+        --output_dir  /HDD_DISK/users/mazipei/BZG/dataset/017/seg_npy \
+        --box_threshold 0.3 \
+        --text_threshold 0.25 \
+        --text_prompt "$text_prompt" \
+        --device "cuda" \
+        --file_pattern "*_0.png" &
 
-# GPU 2 处理相机2的图像
-CUDA_VISIBLE_DEVICES=2 python grounded_sam_demo.py \
-    --config GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py \
-    --grounded_checkpoint groundingdino_swint_ogc.pth \
-    --sam_checkpoint sam_vit_h_4b8939.pth \
-    --image_dir /HDD_DISK/users/mazipei/BZG/dataset/703/images \
-    --output_dir  /HDD_DISK/users/mazipei/BZG/dataset/703/seg_npy \
-    --box_threshold 0.3 \
-    --text_threshold 0.25 \
-    --text_prompt "car" \
-    --device "cuda" \
-    --file_pattern "*_2.png" &
+    # GPU 1 处理相机1的图像
+    CUDA_VISIBLE_DEVICES=6 python grounded_sam_demo.py \
+        --config GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py \
+        --grounded_checkpoint groundingdino_swint_ogc.pth \
+        --sam_checkpoint sam_vit_h_4b8939.pth \
+        --image_dir /HDD_DISK/users/mazipei/BZG/dataset/017/images \
+        --output_dir  /HDD_DISK/users/mazipei/BZG/dataset/017/seg_npy \
+        --box_threshold 0.3 \
+        --text_threshold 0.25 \
+        --text_prompt "$text_prompt" \
+        --device "cuda" \
+        --file_pattern "*_1.png" &
 
-# 等待所有后台进程完成
-wait
+    # GPU 2 处理相机2的图像
+    CUDA_VISIBLE_DEVICES=7 python grounded_sam_demo.py \
+        --config GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py \
+        --grounded_checkpoint groundingdino_swint_ogc.pth \
+        --sam_checkpoint sam_vit_h_4b8939.pth \
+        --image_dir /HDD_DISK/users/mazipei/BZG/dataset/017/images \
+        --output_dir  /HDD_DISK/users/mazipei/BZG/dataset/017/seg_npy \
+        --box_threshold 0.3 \
+        --text_threshold 0.25 \
+        --text_prompt "$text_prompt" \
+        --device "cuda" \
+        --file_pattern "*_2.png" &
+
+    # 等待当前文本提示的所有后台进程完成
+    wait
+    
+    echo "文本提示 '$text_prompt' 处理完成"
+done
+
+echo "所有处理已完成"
